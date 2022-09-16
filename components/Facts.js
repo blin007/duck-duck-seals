8c79188b5e7d82596d23315aa47fbf892df848cb
Brian Lin <blin00712@gmail.com>
2022-09-16 14:03:22 -0400

    9/16 facts section and animations

--- components/Facts.js
+++ components/Facts.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import Slider from './Slider'
 
-const Facts = () => {
+import { motion } from 'framer-motion'
+
+import { container, fadeInFromLeft, fadeInFromRight } from '../variants'
+
+const Facts = ({ factData }) => {
+  const { title, facts } = factData
   return (
-    <div>Facts</div>
+    <section className='min-h-[200px] mt-[20px] mb-[20px]'>
+      <div className='container mx-auto'>
+        <motion.div 
+          variants={container}
+          initial="initial"
+          whileInView={"animate"}
+          viewport={{ once: false, amount: 0.5 }}
+          className='flex flex-col lg:flex-row items-center justify-center'
+        >
+          <motion.div
+            variants={fadeInFromRight} 
+            className='lg:w-[20%] sm:mb-[20px]'
+          >
+            <h3 className='text-primary font-bold'>{title}</h3>
+          </motion.div>
+          <motion.div variants={fadeInFromLeft} className='w-[80%] '>
+            <Slider facts={facts}/>
+          </motion.div>
+        </motion.div>
+      </div>
+    </section>
   )
 }
 
--- components/Slider.js
+++ components/Slider.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Slider = () => {
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+const Slider = ({ facts }) => {
   return (
-    <div>Slider</div>
+    <Swiper slidesPerView={1} grabCursor={true} spaceBetween={20} pagination={true} modules={[Pagination]} className="slider">
+      {facts.map((f, i) => {
+        const { fact } = f
+        return (
+          <SwiperSlide key={i}>
+            <div className='bg-white rounded-lg p-10 py-20 '>
+              <p className='font-light hover:drop-shadow-sm'>{fact}</p>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
   )
 }
 
